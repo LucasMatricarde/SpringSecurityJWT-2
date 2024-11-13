8010997bc780c472300a50026dc1006988571c7a
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-11-13 20:50:17 -0300

    Adicionando JWTCreator e JWTFilter

--- src/main/java/lucasmatricarde/com/springsecurityjwt2/Security/SecurityConfig.java
+++ src/main/java/lucasmatricarde/com/springsecurityjwt2/Security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package lucasmatricarde.com.springsecurityjwt2.Security;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -10,10 +11,39 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@ConfigurationProperties(prefix = "security.config")
 public class SecurityConfig {
+
+    public static String PREFIX;
+    public static String KEY;
+    public static Long EXPIRATION;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    public static String getPREFIX() {
+        return PREFIX;
+    }
+
+    public static void setPREFIX(String PREFIX) {
+        SecurityConfig.PREFIX = PREFIX;
+    }
+
+    public static String getKEY() {
+        return KEY;
+    }
+
+    public static void setKEY(String KEY) {
+        SecurityConfig.KEY = KEY;
+    }
+
+    public static Long getEXPIRATION() {
+        return EXPIRATION;
+    }
+
+    public static void setEXPIRATION(Long EXPIRATION) {
+        SecurityConfig.EXPIRATION = EXPIRATION;
     }
 }
